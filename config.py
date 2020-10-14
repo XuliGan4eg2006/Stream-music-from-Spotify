4fc608b3cc3fe697ed37a0b7b42fa4f9cb122207
XuliGan4eg2006 <62839426+XuliGan4eg2006@users.noreply.github.com>
2020-10-14 12:06:13 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#====Telegram====
+api_id = your api_id #my.telegram.org
+api_hash = 'your api_hash' #my.telegram.org
+#====Spotify==== All lines in https://developer.spotify.com/dashboard/applications/
+client_id = "your client_id"
+client_secret = "your client_secret"
+redirect_uri = "http://localhost:8888/callback" #Dont touch!!!
+spotiusername = "your name in Spotify"
